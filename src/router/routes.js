cbb924b8cfe4e9b360038559a351eaeed63ca31a
Vanetta P Silveira <149812160+Vanettas@users.noreply.github.com>
2025-07-23 14:49:01 +0530

    Add files via upload

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,12 +5,25 @@ export default [
     children: [
       {
         path: '',
+        name: 'home',
         component: () => import('pages/HomePage.vue')
       },
       {
-        path: '/form',
+        path: 'form',
+        name: 'form',
         component: () => import('pages/form.vue')
+      },
+      {
+        path: 'cform',
+        name: 'cform',
+        component: () => import('pages/cform.vue')
       }
     ]
+  },
+
+  // Optional: catch-all route for 404s
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue')
   }
 ]
